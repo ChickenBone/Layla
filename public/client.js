5d2da3dc93c6c51d40b0782977b5a2d7dccb08c8
ChickenBone <bluebomber019@gmail.com>
2019-11-08 16:28:30 -0700

    Alot

--- public/client.js
+++ public/client.js
@@ -1,31 +1,19 @@
 console.log("Wait thats not allowed");
-var place = document.getElementById("boxplace");
-var reasons = [
-  "Your Smile is The Most Beautiful Thing On This Planet",
-  "You Treat Me Amazingly and Respect and Love Me",
-  "I Know I Can Grow With You",
-  "You are the Nicest Person and Work With Everyone no Matter What",
-  "Everyday and every second I hang out with you it is as good as the first time I fell in love with you",
-  "You accept and love how cheesy I am",
-  "You look forward to every day with a smile and stay happy even when the world is against you",
-  "Even if I make you mad or if you hate me for sometime I know you'll be there for me in the end",
-  "You are an amazing person to talk to and one of the few people I have been able to trust fully in a long time",
-  "You have amazing taste in everything, except for makeup, that shouldn't ever be put on me lmaoo",
-  "You are there for me when I am sad to the point where if I'm sad your sad and that really means the world to me",
-  "You are really just the most amazing person for me and I am the happiest guy alive to call you mine"
-];
-var genoratedHTML = "";
-reasons.forEach((reason, i) => {
-  genoratedHTML += `<div class="row">
+$.getJSON("data.json", function (json) {
+  var place = document.getElementById("boxplace");
+  var reasons = json.items
+  var genoratedHTML = "";
+  reasons.forEach((reason, i) => {
+    genoratedHTML += `<div class="row">
         <div class="col box box${Math.floor(Math.random() * 5) + 1}">
           <h3>
             reason ${i + 1}
           </h3>
           <h5>
-            ${reason.toLowerCase()}
+            ${reason.text.toLowerCase()}
           </h5>
         </div>
       </div>`;
-});
-
-place.innerHTML = genoratedHTML;
+  });
+  place.innerHTML = genoratedHTML;
+})
--- server.js
+++ server.js
@@ -5,8 +5,11 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-var bodyParser = require("body-parser");
-app.use(bodyParser.json());
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // support encoded bodies
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -15,15 +18,27 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
-
-app.post("/add", function(req, res) {
-  res.send(req.body);
+app.get("/add", function(req, res){
+  res.sendFile(__dirname + "/views/add.html")
+})
+app.post("/add", function (req, res) {
+  if(req.body.text){
+    fs.readFile("./public/data.json", (err, data)=>{
+      data = JSON.parse(data)
+      data.items.push({text: req.body.text})
+      fs.writeFile("./public/data.json",JSON.stringify(data),()=>{
+        res.send({"success": "text added"})
+      })
+    })
+  }else{
+    res.send({"err": "no text"})
+  }
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(80, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
