a13aad3a6c1d9ea36db46a2421923e52d12160ce
ChickenBone <bluebomber019@gmail.com>
2019-11-08 16:43:30 -0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -3,17 +3,17 @@ $.getJSON("data.json", function (json) {
   var place = document.getElementById("boxplace");
   var reasons = json.items
   var genoratedHTML = "";
-  reasons.forEach((reason, i) => {
+  for(var i = reasons.length; i--;){
     genoratedHTML += `<div class="row">
         <div class="col box box${Math.floor(Math.random() * 5) + 1}">
           <h3>
             reason ${i + 1}
           </h3>
           <h5>
-            ${reason.text.toLowerCase()}
+            ${reasons[i].text.toLowerCase()}
           </h5>
         </div>
       </div>`;
-  });
+  }
   place.innerHTML = genoratedHTML;
 })
