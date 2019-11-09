816ac3e6c4a92b1ae9e6f0f06fc2f52b125c4cf8
ChickenBone <bluebomber019@gmail.com>
2019-11-08 17:00:28 -0700

    stuff

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require("express");
 const app = express();
 const fs = require("fs");
 var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-})); // support encoded bodies
-
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
+}));
 app.use(express.static("public"));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
@@ -37,8 +26,6 @@ app.post("/add", function (req, res) {
     res.send({"err": "no text"})
   }
 });
-
-// listen for requests :)
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
